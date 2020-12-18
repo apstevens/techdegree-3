abf94c4a80a569afdd599cc52cfbfd2514b90dc6
Andy Stevens <57344820+apstevens@users.noreply.github.com>
2020-12-18 08:28:32 +0000

    added js functionality

--- js/script.js
+++ js/script.js
@@ -22,23 +22,25 @@ const shirtDesign = () => {
     const color = document.querySelector('#color');
     const choice = document.querySelectorAll('#color option');
 
-    console.log(design);
-    console.log(color);
-    console.log(choice)
+    // console.log(design);
+    // console.log(color);
+    // console.log(choice)
 
     color.disabled = true;
 
-    design.addEventListener('change', (e) => {
+    design.addEventListener('change', () => {
 
-        for (let i = 0; i < design.length; i++){
-            let option = e.target.value;
-            let design_choice = design[i];
+        color.disabled = false;
+
+        for (let i = 0; i < choice.length; i++){
+            const option = choice[i].getAttribute('data-theme');
+            const design_choice = design.value;
 
             console.log(option);
-            console.log(design_choice);
+            // console.log(design_choice);
 
-            if (option === design_choice[1]) {
-                color.disabled = false;
+            if (option === design_choice) {
+                option.selected = true;
             }
         }
         
